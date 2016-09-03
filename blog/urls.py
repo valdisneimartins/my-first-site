3ade5747d8b25eaaa2138cd28b5410bcecdd5cec
valdisnei <maldisneimartins@gmail.com>
2016-09-03 10:54:51 -0300

    modifica o site

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 	url(r'^$', views.post_list),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
 	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/$', views.post_detail,name='post_detail'),
 	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 	url(r'^post/(?P<pk>\d+)/comment/$',views.add_comment_to_post, name='add_comment_to_post'),
 	url(r'^post/(?P<pk>\d+)/remove/$',views.post_remove,name="post_remove"),
